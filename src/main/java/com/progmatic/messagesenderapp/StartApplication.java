b58f34da9709fa7860bce68bc4202b49be9d6439
Pálmai Mercédesz <palmaim@hotmail.com>
2019-06-20 11:50:04 +0200

    start

---
+++ src/main/java/com/progmatic/messagesenderapp/StartApplication.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.progmatic.messagesenderapp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ *
+ * @author progmatic
+ */
+
+@SpringBootApplication
+@ComponentScan("com.progmatic.messagesenderapp")
+public class StartApplication {
+    
+}
